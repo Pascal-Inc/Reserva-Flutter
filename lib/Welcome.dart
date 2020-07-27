ce31856c75457a097e19091249cc522f4ab40e6f
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2020-07-27 21:34:20 +0800

    Update Welcome.dart

--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Reserva App',
       theme: ThemeData(
         // This is the theme of your application.
