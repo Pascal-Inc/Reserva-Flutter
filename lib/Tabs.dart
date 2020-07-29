302f303e3d58ce2771a7e7825e34dd7e2909f995
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2020-07-29 23:36:35 +0800

    Update Tabs.dart

--- lib/Tabs.dart
+++ lib/Tabs.dart
@@ -87,7 +87,29 @@ class TabBarClass extends StatelessWidget {
                         child: ListTile(
                           leading: FlutterLogo(size: 72.0),
                           title: Text('Singapore Food Festival'),
-                          subtitle: Text('Food\n10/7/20 - 26/7/20'),
+                          subtitle: Text('Food\n11/7/20 - 20/7/20 (Annually)'),
+                          trailing: Text('4.6'),
+                          /*
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow[600],
+                              ),
+                              Text('3.9')
+                            ],
+                          ),
+                          */
+                          isThreeLine: true,
+                          onTap: () {},
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: FlutterLogo(size: 72.0),
+                          title: Text('Grand Prix Singapore'),
+                          subtitle: Text('Races\n10/7/20 - 26/7/20 (Annually)'),
                           // trailing: Icon(Icons.more_vert),
                           trailing: Text('3.9'),
                           /*
@@ -103,6 +125,55 @@ class TabBarClass extends StatelessWidget {
                           ),
                           */
                           isThreeLine: true,
+                          onTap: () {},
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: FlutterLogo(size: 72.0),
+                          title: Text('Singapore Art Week'),
+                          subtitle: Text(
+                              'Charity and Causes\n15/9/20 - 22/9/20 (Biannually)'),
+                          // trailing: Icon(Icons.more_vert),
+                          trailing: Text('4.2'),
+                          /*
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow[600],
+                              ),
+                              Text('3.9')
+                            ],
+                          ),
+                          */
+                          isThreeLine: true,
+                          onTap: () {},
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: FlutterLogo(size: 72.0),
+                          title: Text('Masks Sewn With Love'),
+                          subtitle: Text(
+                              'Charity and Causes\nDuring COVID-19 (Weekly)'),
+                          // trailing: Icon(Icons.more_vert),
+                          trailing: Text('5.0'),
+                          /*
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow[600],
+                              ),
+                              Text('3.9')
+                            ],
+                          ),
+                          */
+                          isThreeLine: true,
+                          onTap: () {},
                         ),
                       ),
                       /*
@@ -166,3 +237,11 @@ class TabBarClass extends StatelessWidget {
             )));
   }
 }
+
+/*
+Swift Code for reference:
+Event(eventImage: "placeholder_image", eventName: "Singapore Food Festival", rating: 4.6, userLiked: true, eventDescription: "Fuelled by our national obsession, the Singapore Food Festival boasts tasty dishes, cooking tips and unique dining experiences", eventStartDate: "10/7/20", eventEndDate: "26/7/20", eventCategory: "Food", eventFrequency: "Annually"),
+Event(eventImage: "placeholder_image", eventName: "Grand Prix Singapore", rating: 3.9, userLiked: false, eventDescription: "A motor race which forms part of the FIA Formula One World Championship", eventStartDate: "11/7/20", eventEndDate: "20/7/20", eventCategory: "Races", eventCost: "$38", eventCostTimeLengthInDays: 1, eventFrequency: "Annually"),
+Event(eventImage: "placeholder_image", eventName: "Singapore Art Week", rating: 4.2, userLiked: false, eventDescription: "15/9/20", eventStartDate: "22/9/20", eventEndDate: "22/9/20", eventCategory: "Art", eventFrequency: "Biannually"),
+Event(eventImage: "placeholder_image", eventName: "Masks Sewn With Love", rating: 5.0, userLiked: true, eventDescription: "A community project to sew reusable cloth masks", eventCategory: "Charity and Causes", eventFrequency: "Weekly")
+*/
