3a8b00f8d5b2de89fd3965151626450cd8bd8cc9
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2020-07-29 17:29:30 +0800

    Add New Features
    Mainly New Table, Starting Screen is a new class

--- lib/Tabs.dart
+++ lib/Tabs.dart
@@ -4,30 +4,90 @@ class TabBarClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              bottom: TabBar(tabs:
-                [
-                  Tab(icon: Icon(Icons.calendar_today)),
-                  Tab(icon: Icon(Icons.event)),
-                  Tab(icon: Icon(Icons.search)),
-                  Tab(icon: Icon(Icons.account_circle),)
+        debugShowCheckedModeBanner: false,
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              appBar: AppBar(
+                bottom: TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.calendar_today)),
+                    Tab(icon: Icon(Icons.event)),
+                    Tab(icon: Icon(Icons.search)),
+                    Tab(
+                      icon: Icon(Icons.account_circle),
+                    )
+                  ],
+                ),
+                title: Text('Reserva'),
+              ),
+              body: TabBarView(
+                children: [
+                  DataTable(
+                    columns: const <DataColumn>[
+                      DataColumn(
+                        label: Text(
+                          'Name',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          'Date',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          'Category',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          'Rating',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                    rows: const <DataRow>[
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text('Singapore Food Festival')),
+                          DataCell(Text('10/7/20-26/7/20')),
+                          DataCell(Text('Food')),
+                          DataCell(Text("4.6"))
+                        ],
+                      ),
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text('Grand Prix Singapore')),
+                          DataCell(Text('11/7/20-20/7/20')),
+                          DataCell(Text('Races, \$38')),
+                          DataCell(Text("3.9"))
+                        ],
+                      ),
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text('Singapore Art Week')),
+                          DataCell(Text('15/9/20-22/9/20')),
+                          DataCell(Text('Charity and Causes')),
+                          DataCell(Text("4.2"))
+                        ],
+                      ),
+                    ],
+                  ),
+                  Text("All Events"),
+                  Text("Search"),
+                  Text("Settings")
+                  /*
+                  Icon(Icons.calendar_today),
+                  Icon(Icons.event),
+                  Icon(Icons.search),
+                  Icon(Icons.account_circle)
+                  */
                 ],
               ),
-              title: Text('Tabs'),
-            ),
-            body: TabBarView(
-              children: [
-                Icon(Icons.calendar_today),
-                Icon(Icons.event),
-                Icon(Icons.search),
-                Icon(Icons.account_circle)
-              ],
-            ),
-          )
-      )
-    );
+            )));
   }
 }
--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reserva_flutter/Tabs.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,26 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Reserva Flutter Home Page',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Reserva Flutter Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Reserva Flutter Home Page',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.green,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: TabBarClass() // MyHomePage(title: 'Reserva Flutter Home Page'),
+        );
   }
 }
 
