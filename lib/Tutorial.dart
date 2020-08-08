35243884be685e61ca905d4a8f0e8ae789e71584
adam061606 <62824117+adam061606@users.noreply.github.com>
2020-08-09 00:25:27 +0800

    Update Tutorial.dart

--- lib/Tutorial.dart
+++ lib/Tutorial.dart
@@ -1,8 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+import 'package:reserva_flutter/Welcome.dart';
 
-class TutorialScreen extends StatelessWidget {
+void main() => runApp(new MaterialApp(
+      theme:
+          ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.blueAccent),
+          ),
+          Column(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: Colors.greenAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 10.0)),
+                      Text(
+                        "flutKart",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(child: null)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
